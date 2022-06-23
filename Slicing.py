b4abec7076eb4e4fd950564296be915d7c8ca3e8
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-06-23 15:08:20 +0530

    Add files via upload

---
+++ Slicing.py
@@ -0,0 +1,14 @@
+x="python"
+# print(x[1:5]) #ytho
+# print(x[2:5]) #tho 
+# print(x[4:5]) #o
+# print(x[5:5]) 
+# print(x[1:4]) #yth
+# print(x[5:4])
+# print(x[3:4]) #h
+# print(x[3:3])
+# print(x[5:3])
+# print(x[ :3]) #pyt
+# print(x[ :1]) #p
+# print(x[4: ]) #on
+# print(x[5: ]) #n
